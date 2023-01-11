bf7f7a210863a8746acc3bc5a180dc9418dc9004
divyashantkumarCS <divyashant@checkedspot.com>
2023-01-11 18:51:07 +0530

    11-Jan

--- findhouses/myjs/postProp.js
+++ findhouses/myjs/postProp.js
@@ -5,3 +5,26 @@
 function selector(event) {
     // console.log(event.target.value);
 }
+
+function personalDetail(arg) {
+    if(arg === 'owner'){
+        document.getElementById("perDetFormCont").innerHTML = 
+        `<div class="name">
+            <p class="customLabel">Name</p>
+            <input type="text" name="name" id="name" placeholder="Enter Your Name">
+        </div>
+        <div class="mobile">     
+            <p class="customLabel">Mobile</p>   
+            <div class="mobCont">
+                <p>IND +91</p>
+                <input type="text" id="mob" name="mobile" placeholder="Enter Your Mobile">               
+            </div> 
+        </div>
+        <div class="email">
+            <p class="customLabel">Email</p>
+            <input type="text" name="email" id="email" placeholder="Enter Your Email">
+        </div>`
+    }else {
+        document.getElementById("perDetFormCont").innerHTML = ""
+    }
+}
